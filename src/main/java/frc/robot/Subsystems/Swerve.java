b82b2b42231d00e297538a39f7533111ebcc29c5
bmccarthysjc <bmccarthy@stjohnschs.org>
2023-01-21 12:08:21 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/Subsystems/Swerve.java
@@ -0,0 +1,153 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+
+public class Swerve extends SubsystemBase {
+  private final AHRS m_gyro;
+
+  private SwerveDriveOdometry swerveOdometry;
+  private SwerveModule[] mSwerveMods;
+
+  private Field2d field;
+
+  public Swerve() {
+    m_gyro = new AHRS(SPI.Port.kMXP);
+    //.configFactoryDefault();
+    zeroHeading();
+
+    mSwerveMods =
+        new SwerveModule[] {
+          new SwerveModule(0, Constants.Swerve.Mod0.constants),
+          new SwerveModule(1, Constants.Swerve.Mod1.constants),
+          new SwerveModule(2, Constants.Swerve.Mod2.constants),
+          new SwerveModule(3, Constants.Swerve.Mod3.constants)
+        };
+
+    Timer.delay(1.0);
+    swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getRotation2d(), getModulePositions());
+
+    field = new Field2d();
+    SmartDashboard.putData("Field", field);
+  }
+
+  public void drive(
+      Translation2d translation, double rotation, /*boolean fieldRelative,*/ boolean isOpenLoop) {
+    SwerveModuleState[] swerveModuleStates =
+        Constants.Swerve.swerveKinematics.toSwerveModuleStates(
+            ChassisSpeeds.fromFieldRelativeSpeeds(
+                    translation.getX(), translation.getY(), rotation, getRotation2d()));
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
+
+    for (SwerveModule mod : mSwerveMods) {
+      mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+    }
+  }
+/*  Drive with field relative boolean
+  public void drive(
+    Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+  SwerveModuleState[] swerveModuleStates =
+      Constants.Swerve.swerveKinematics.toSwerveModuleStates(
+          fieldRelative
+              ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                  translation.getX(), translation.getY(), rotation, getYaw())
+              : new ChassisSpeeds(translation.getX(), translation.getY(), rotation));
+  SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
+
+  for (SwerveModule mod : mSwerveMods) {
+    mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+  }
+}
+*/
+
+  /* Used by SwerveControllerCommand in Auto */
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.Swerve.maxSpeed);
+
+    for (SwerveModule mod : mSwerveMods) {
+      mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+    }
+  }
+
+  public Pose2d getPose() {
+    return swerveOdometry.getPoseMeters();
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    swerveOdometry.resetPosition(getRotation2d(), getModulePositions(), pose);
+  }
+
+  public SwerveModuleState[] getStates() {
+    SwerveModuleState[] states = new SwerveModuleState[4];
+    for (SwerveModule mod : mSwerveMods) {
+      states[mod.moduleNumber] = mod.getState();
+    }
+    return states;
+  }
+
+  public void zeroHeading() {
+    m_gyro.reset();
+  }
+
+  public double getHeading() {
+    return Math.IEEEremainder(m_gyro.getAngle(), 360);
+}
+
+  public Rotation2d getRotation2d() {
+    return m_gyro.getRotation2d();
+  }
+/*
+public Rotation2d getRotation2d() {
+    return Rotation2d.fromDegrees(getHeading());
+}
+*/
+
+ /* 
+  public Rotation2d getYaw() {
+    return (Constants.Swerve.invertGyro)
+        ? Rotation2d.fromDegrees(360 - gyro.getYaw())
+        : Rotation2d.fromDegrees(gyro.getYaw());
+  }
+  */
+
+  public SwerveModulePosition[] getModulePositions(){
+    SwerveModulePosition[] positions = new SwerveModulePosition[4];
+    for(SwerveModule mod : mSwerveMods){
+        positions[mod.moduleNumber] = mod.getPosition();
+    }
+    return positions;
+}
+
+  @Override
+  public void periodic() {
+    swerveOdometry.update(getRotation2d(), getModulePositions());
+    field.setRobotPose(getPose());
+
+    for (SwerveModule mod : mSwerveMods) {
+      SmartDashboard.putNumber(
+          "Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
+      SmartDashboard.putNumber(
+          "Mod " + mod.moduleNumber + " Integrated", mod.getState().angle.getDegrees());
+      SmartDashboard.putNumber(
+          "Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);
+    }
+  }
+}
